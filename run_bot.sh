3d3ac79031368bc8969312704aa7d180952c7457
Juwan-s <dlacksdn352@gmail.com>
2025-05-20 09:33:44 +0900

    [FEAT] shell file for the one click start

---
+++ run_bot.sh
@@ -0,0 +1,4 @@
+ros2 launch turtlebot3_manipulation_bringup hardware.launch.py
+ros2 launch aruco_yolo aruco_yolo.launch.py
+ros2 launch turtlebot3_manipulation_moveit_config moveit_core.launch.py
+ros2 run turtlebot_moveit turtlebot_arm_controller
