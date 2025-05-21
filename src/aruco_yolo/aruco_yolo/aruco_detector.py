2499a6055e5d29094eeed4b161c8bd5c331626ba
Juwan-s <dlacksdn352@gmail.com>
2025-05-21 10:34:45 +0900

    [UPDATE] aruco detected image publish

--- src/aruco_yolo/aruco_yolo/aruco_detector.py
+++ src/aruco_yolo/aruco_yolo/aruco_detector.py
@@ -11,6 +11,7 @@ from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import Float32, Int32
 from aruco_msgs.msg import Marker, MarkerArray
 from cv_bridge import CvBridge
+import time
 
 def detect_markers(image, camera_matrix, dist_coeffs, marker_size):
     aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_1000)
@@ -81,11 +82,12 @@ class ArucoMarkerDetector(Node):
             10)
 
         self.marker_publisher = self.create_publisher(MarkerArray, 'detected_markers', 10)
+        self.img_publisher = self.create_publisher(CompressedImage, 'aruco/compressed', 1)
         
         self.bridge = CvBridge()
         self.marker_size = 0.04
         self.camera_matrix, self.dist_coeffs = load_camera_parameters('calibration_params.yaml')
-        
+        self.last_pub_time = time.time()
         # self.camera_matrix  = []
         # self.dist_coeffs = []
 
@@ -116,8 +118,31 @@ class ArucoMarkerDetector(Node):
                 marker_msg.pose.pose.orientation.z = marker[2][0]
                 marker_array_msg.markers.append(marker_msg)
             self.marker_publisher.publish(marker_array_msg)
+        self.publish_img(frame)
         # cv2.imshow('Detected Markers', frame)
         # cv2.waitKey(1)
+
+    def publish_img(self, frame):
+        time_cur = time.time()
+
+        if time_cur - 0.2 < self.last_pub_time:
+            return
+
+        self.last_pub_time = time_cur
+
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 30]  # 90은 압축 품질
+        _, compressed_image = cv2.imencode('.jpg', frame, encode_param)
+
+        # 압축된 이미지를 CompressedImage 메시지로 변환
+        msg = CompressedImage()
+        msg.header.stamp = self.get_clock().now().to_msg()  # 타임스탬프 추가
+        msg.header.frame_id = "camera"  # 프레임 ID 설정
+        msg.format = "jpeg"  # 압축 형식 설정
+        msg.data = compressed_image.tobytes()  # 압축된 이미지 데이터
+
+        # CompressedImage 퍼블리시
+        self.img_publisher.publish(msg)
+        # self.get_logger().info('Publishing compressed image...')
 
 def main(args=None):
     rclpy.init(args=args)
